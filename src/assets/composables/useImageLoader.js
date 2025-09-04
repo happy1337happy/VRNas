511ca6978cd46118e73428b2fa2dca28d9380f2b
happy1337happy <love.stone1337@gmail.com>
2025-09-04 15:05:38 +0300

    Fix base and image paths

--- src/assets/composables/useImageLoader.js
+++ src/assets/composables/useImageLoader.js
@@ -1,10 +1,7 @@
 const allImages = import.meta.glob('/src/assets/img/**/*.{jpg,png,jpeg,svg,webp}', { eager: true });
 
 export function useImageLoader(fileName) {
-  console.log('allImages keys:', Object.keys(allImages));
-  console.log('fileName:', fileName);
-  const key = Object.keys(allImages).find(k => k.endsWith(fileName));
-  console.log('matched key:', key);
+  const key = Object.keys(allImages).find(k => k.includes(fileName));
   return key ? allImages[key].default : '';
 }
 
