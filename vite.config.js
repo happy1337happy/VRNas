f1f4ca4ba298848396df3818838d40a1d2013664
happy1337happy <love.stone1337@gmail.com>
2025-09-04 21:27:45 +0300

    Fix routing with base path /VRNas/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [vue()],
-    base: command === 'build' ? '/VRNas/' : '/', 
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+export default defineConfig({
+  base: '/VRNas/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  }
-})
+  },
+});
