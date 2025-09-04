f2b8e68d5db4d5b6f2e1f76e6ee7f6d7aef8e7c1
happy1337happy <love.stone1337@gmail.com>
2025-09-04 10:23:55 +0300

    Update vite.config.js and package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  base: '/VRNas/',
-  css:{
-    preprocessorOptions:{
-      scss:{
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === 'production' ? '/VRNas/' : '/',
+    css: {
+      preprocessorOptions: {
+        scss: {},
       },
     },
-  },
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname,'./src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  };
 })
